6fdda11c3050ad6702b74e367f92f7e543c3c5a4
stefanyds <ste.stefany22@gmail.com>
2024-03-13 18:00:01 -0300

    feat: added more Supabase configurations to the services folder

--- cook_app/src/services/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as ingredients from "./ingredientsServices"
-
-export const services = {
-    ingredients,
-
-    storage: {
-        imagePath:"https://mwdbgbfsrncqeiozebmj.supabase.co/storage/v1/object/public/ingredients"
-    }
-}
